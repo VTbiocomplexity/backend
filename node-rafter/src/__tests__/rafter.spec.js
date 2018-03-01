285a165fcdc16730dc1fcc9d429edc034fb86ad0
Joshua Sherman <joshua.v.sherman@gmail.com>
2018-03-01 14:44:04 -0500

    rafter node from npm

--- node-rafter/src/__tests__/rafter.spec.js
+++
@@ -1,9 +0,0 @@
-let RafterService = require('../rafter');
-
-describe('RafterService', () => {
-  it('can be created', () => {
-    let r = new RafterService('waddup', 'token');
-    expect(r.url).toBe('waddup');
-    expect(r.token).toBe('token');
-  });
-});
