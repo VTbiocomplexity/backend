4ea23e0a6eb3cb6e7600ec3923f3f1899bb716a0
Joshua Sherman <joshua.v.sherman@gmail.com>
2018-03-19 14:42:15 -0400

    rafter init and removed login for now

--- test/unit/rafer_unit.js
+++ test/unit/rafer_unit.js
@@ -1,50 +1,51 @@
 const rafter = require('../../rafter/rController');
 // const VolumeService = require('rafter').VolumeService;
-const nock = require('nock');
+// const nock = require('nock');
 // VolumeService.list = function() { return promise.resolve({ json: () => Promise.resolve({ name: 'filename' }) }); };
 describe('The Unit Test for Rafter', () => {
 
-  it('should login a rafter user', (done) => {
-    nock('https://rafter.bi.vt.edu')
-      .defaultReplyHeaders({
-        'set-cookie':['cookie']
-      })
-      .post('/usersvc/login')
-      .reply(301);
-      const token = { token: '<div></div>' };
-    nock('https://rafter.bi.vt.edu')
-      .get('/usersvc/')
-      .reply(200, token);
-    const req = { body: { id:'yo', password: 'yo' } };
-    const res = {
-        json: (msg) => {
-        expect(msg).to.equal('{"token":"<div></div>"}');
-        done();
-      }
-    };
-    rafter.rlogin(req, res);
-  });
+  // it('should login a rafter user', (done) => {
+  //   nock('https://rafter.bi.vt.edu')
+  //     .defaultReplyHeaders({
+  //       'set-cookie':['cookie']
+  //     })
+  //     .post('/usersvc/login')
+  //     .reply(301);
+  //     const token = { token: '<div></div>' };
+  //   nock('https://rafter.bi.vt.edu')
+  //     .get('/usersvc/')
+  //     .reply(200, token);
+  //   const req = { body: { id:'yo', password: 'yo' } };
+  //   const res = {
+  //       json: (msg) => {
+  //       expect(msg).to.equal('{"token":"<div></div>"}');
+  //       done();
+  //     }
+  //   };
+  //   rafter.rlogin(req, res);
+  // });
+  //
+  // it('should not login a rafter user', (done) => {
+  //   nock('https://rafter.bi.vt.edu')
+  //     .defaultReplyHeaders({
+  //       'set-cookie':['cookie']
+  //     })
+  //     .post('/usersvc/login')
+  //     .reply(400);
+  //   const token = { token: '<div></div>' };
+  //   nock('https://rafter.bi.vt.edu')
+  //     .get('/usersvc/')
+  //     .reply(200, token);
+  //   const req = { body: { id:'yo', password: 'yo' } };
+  //   const res = {
+  //     status: (code) => {
+  //       expect(code).to.equal(400);
+  //       done();
+  //     }
+  //   };
+  //   rafter.rlogin(req, res);
+  // });
 
-  it('should not login a rafter user', (done) => {
-    nock('https://rafter.bi.vt.edu')
-      .defaultReplyHeaders({
-        'set-cookie':['cookie']
-      })
-      .post('/usersvc/login')
-      .reply(400);
-    const token = { token: '<div></div>' };
-    nock('https://rafter.bi.vt.edu')
-      .get('/usersvc/')
-      .reply(200, token);
-    const req = { body: { id:'yo', password: 'yo' } };
-    const res = {
-      status: (code) => {
-        expect(code).to.equal(400);
-        done();
-      }
-    };
-    rafter.rlogin(req, res);
-  });
   it('initializes the volume service', async() => {
     const req = { body: { command: 'ls', token: 'token', userName: 'yoyo', rafterFile: { path: '/yo' } } };
     const res = {
