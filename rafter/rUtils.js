80380b6a84b9299ce591af6c10af891e33e305cc
Joshua Sherman <joshua.v.sherman@gmail.com>
2018-05-01 13:14:36 -0400

    linting

--- rafter/rUtils.js
+++ rafter/rUtils.js
@@ -2,26 +2,21 @@ class AuthUtils {
   static handleRafterAppId(existingUser, myId, mySecret, myAppName) {
     for (let i = 0; i < existingUser.rafterApps.length; i += 1) {
       if (existingUser.rafterApps[i].r_app_id === myId) {
-        // found = true;
-        console.log('I found an app id');
-        // if (existingUser.rafterApps[i].r_app_secret !== mySecret) {
         existingUser.rafterApps.splice(i, 1);
-        // updateSecret = true;
-        // console.log('change the app secret');
-        // console.log(mySecret);
-        // found = false;
-        // }
       }
     }
-    // if (!found) {
     existingUser.rafterApps.push({ r_app_id: myId, r_app_secret: mySecret, r_app_name: myAppName });
     return existingUser;
   }
   static handleVsCreate(req, res, vs) {
-    if (req.body.command === 'create' && (req.body.rafterFile.name === '' || req.body.rafterFile.name === null || req.body.rafterFile.name === undefined)) {
+    if (req.body.command === 'create' && (req.body.rafterFile.name === '' || req.body.rafterFile.name === null ||
+    req.body.rafterFile.name === undefined)) {
       return res.status(400).json({ error: 'Invalid request: missing file/folder name' });
     } else if (req.body.command === 'create' && req.body.rafterFile.createType === 'file') {
-      vs.create('/home/' + req.body.userName + req.body.rafterFile.path + '/', { name: req.body.rafterFile.name, type: req.body.rafterFile.fileType }).then((data) => {
+      return vs.create('/home/' + req.body.userName + req.body.rafterFile.path + '/', {
+        name: req.body.rafterFile.name,
+        type: req.body.rafterFile.fileType
+      }).then((data) => {
         // console.log(data);
         // console.log('line 62');
         if (req.body.rafterFile.content !== null && req.body.rafterFile.content !== undefined && req.body.rafterFile.content !== '') {
@@ -43,15 +38,15 @@ class AuthUtils {
       // console.log('line79');
       const fullPath = '/home/' + req.body.userName + req.body.rafterFile.path + '/' + req.body.rafterFile.name;
       // console.log(fullPath);
-      vs.mkdir(fullPath, { recursive: true }).then(data =>
+      return vs.mkdir(fullPath, { recursive: true }).then(data =>
         // console.log(data);
         res.json(data)).catch((err) => {
         console.log(err);
         return res.json(err);
       });
-    } else {
-      return res.status(400).json({ error: 'invalid request' });
     }
+    return res.status(400).json({ error: 'invalid request' });
+    
   }
 }
 module.exports = AuthUtils;
